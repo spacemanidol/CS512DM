3c94e9acbb57ec956dd0b6702709bacd3b5f4dbe
Daniel Campos <spacemanidol@gmail.com>
2021-04-19 17:07:39 -0500

    Moving onto question 2

---
+++ Assignments/Assignment2/dcampos3_HW2_problem2.py
@@ -0,0 +1,105 @@
+import os
+import argparse
+import sys
+import numpy as np
+import math
+import scipy.sparse as sp
+
+from mat4py import loadmat
+from pyod.models.vae import VAE
+from pyod.models.pca import 
+data = loadmat('vowels.mat')
+from pyod.models.knn import KNN   # kNN detector
+
+# train kNN detector
+clf_name = 'KNN'
+clf = KNN()
+clf.fit(X_train)
+
+# get the prediction label and outlier scores of the training data
+y_train_pred = clf.labels_  # binary labels (0: inliers, 1: outliers)
+y_train_scores = clf.decision_scores_  # raw outlier scores
+
+# get the prediction on the test data
+y_test_pred = clf.predict(X_test)  # outlier labels (0 or 1)
+y_test_scores = clf.decision_function(X_test) 
+
+# train PCA detector
+    clf_name = 'PCA'
+    clf = PCA(n_components=3)
+    clf.fit(X_train)
+
+    # get the prediction labels and outlier scores of the training data
+    y_train_pred = clf.labels_  # binary labels (0: inliers, 1: outliers)
+    y_train_scores = clf.decision_scores_  # raw outlier scores
+
+    # get the prediction on the test data
+    y_test_pred = clf.predict(X_test)  # outlier labels (0 or 1)
+    y_test_scores = clf.decision_function(X_test)  # outlier scores
+
+    # evaluate and print the results
+    print("\nOn Training Data:")
+    evaluate_print(clf_name, y_train, y_train_scores)
+    print("\nOn Test Data:")
+    evaluate_print(clf_name, y_test, y_test_scores)
+
+     # train VAE detector (Beta-VAE)
+    clf_name = 'VAE'
+    clf = VAE(epochs=30, contamination=contamination, gamma=0.8, capacity=0.2)
+    clf.fit(X_train)
+
+    # get the prediction labels and outlier scores of the training data
+    y_train_pred = clf.labels_  # binary labels (0: inliers, 1: outliers)
+    y_train_scores = clf.decision_scores_  # raw outlier scores
+
+    # get the prediction on the test data
+    y_test_pred = clf.predict(X_test)  # outlier labels (0 or 1)
+    y_test_scores = clf.decision_function(X_test)  # outlier scores
+
+    # evaluate and print the results
+    print("\nOn Training Data:")
+    evaluate_print(clf_name, y_train, y_train_scores)
+    print("\nOn Test Data:")
+    evaluate_print(clf_name, y_test, y_test_scores)
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from matplotlib import pyplot
+# generate 2 class dataset
+X, y = make_classification(n_samples=1000, n_classes=2, random_state=1)
+# split into train/test sets
+trainX, testX, trainy, testy = train_test_split(X, y, test_size=0.5, random_state=2)
+# generate a no skill prediction (majority class)
+ns_probs = [0 for _ in range(len(testy))]
+# fit a model
+model = LogisticRegression(solver='lbfgs')
+model.fit(trainX, trainy)
+# predict probabilities
+lr_probs = model.predict_proba(testX)
+# keep probabilities for the positive outcome only
+lr_probs = lr_probs[:, 1]
+# calculate scores
+ns_auc = roc_auc_score(testy, ns_probs)
+lr_auc = roc_auc_score(testy, lr_probs)
+# summarize scores
+print('No Skill: ROC AUC=%.3f' % (ns_auc))
+print('Logistic: ROC AUC=%.3f' % (lr_auc))
+# calculate roc curves
+ns_fpr, ns_tpr, _ = roc_curve(testy, ns_probs)
+lr_fpr, lr_tpr, _ = roc_curve(testy, lr_probs)
+# plot the roc curve for the model
+pyplot.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+pyplot.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
+# axis labels
+pyplot.xlabel('False Positive Rate')
+pyplot.ylabel('True Positive Rate')
+# show the legend
+pyplot.legend()
+# show the plot
+pyplot.show()
+\
+sklearn.metrics.roc_curve(y_true, y_score, *, pos_label=None, sample_weight=None, drop_intermediate=True)[source]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PYOD3')
+    parser.add_argument('--optim', default='adam', type=str, help='Optimizer. Options are adam, sgd and rmsprop')
+    args = parser.parse_args()
+    main(args)    
